56c7de0760e3f025f125593b107e396b6c982540
_Iyashi <deuxdetrefle@outlook.fr>
2020-03-31 16:04:21 +0200

    formattage

--- ddungeon.py
+++ ddungeon.py
@@ -13,13 +13,14 @@ class Base:
         # dicePos = Liste de tuples pour chaque dé.
         self.dicePos = {}
         self.atkPos = {}
-    
-    def generateDicePos(self, nbDices = 12):
+
+    def generateDicePos(self, nbDices=12):
         # Générer les positions de 12 dés
         self.dicePos.clear()
         for x in range(nbDices):
-            self.dicePos["d{}".format(x+1)] = ((895+((x%6)*145), 955-(145*(x//6))))
-    
+            self.dicePos["d{}".format(
+                x+1)] = ((895+((x % 6)*145), 955-(145*(x//6))))
+
     def generateAtkPos(self, nbAtk):
         # A moins de trouver une solution universelle,
         # faut hardcoder tout ça. ah well.
@@ -31,38 +32,39 @@ class Base:
             pyautogui.moveTo(self.dicePos[x], duration=1)
             sleep(1)
 
+
 class Warrior(Base):
     def __init__(self):
         # Le guerrier commence avec 2 dés
         super().__init__()
         self.generateDicePos()
         self.generateAtkPos(1)
-    
+
     def generateAtkPos(self, nbCols):
         # Le warrior a le reroll.
         # Ecran: 1920x1080, avec la barre des tâches à gauche
         self.atkPos.clear()
         if(nbCols == 1):
-            self.atkPos["atk1"] = (750, 385) # Col 1
+            self.atkPos["atk1"] = (750, 385)  # Col 1
             self.atkPos["atk2"] = (750, 670)
-            self.atkPos["reroll"] = (1220, 550) # Reroll
+            self.atkPos["reroll"] = (1220, 550)  # Reroll
         if(nbCols == 2):
-            self.atkPos["atk1"] = (520, 385) # Col 1
-            self.atkPos["atk2"] = (520, 670) 
-            self.atkPos["atk3"] = (990, 385) # Col 2
-            self.atkPos["atk4"] = (990, 670) 
-            self.atkPos["reroll"] = (1450, 550) # Reroll
+            self.atkPos["atk1"] = (520, 385)  # Col 1
+            self.atkPos["atk2"] = (520, 670)
+            self.atkPos["atk3"] = (990, 385)  # Col 2
+            self.atkPos["atk4"] = (990, 670)
+            self.atkPos["reroll"] = (1450, 550)  # Reroll
         if(nbCols == 3):
-            self.atkPos["atk1"] = (290, 385) # Col 1
-            self.atkPos["atk2"] = (290, 670)  
-            self.atkPos["atk3"] = (750, 385) # Col 2
-            self.atkPos["atk4"] = (750, 670)  
-            self.atkPos["atk5"] = (1210, 385) # Col 3
-            self.atkPos["atk6"] = (1210, 670) 
-            self.atkPos["reroll"] = (1680, 550) # Reroll 
-        self.atkPos["limitbreak"] = (565, 980) # Limitbreak
-        self.atkPos["endturn"] = (1800, 950) # End turn
-    
+            self.atkPos["atk1"] = (290, 385)  # Col 1
+            self.atkPos["atk2"] = (290, 670)
+            self.atkPos["atk3"] = (750, 385)  # Col 2
+            self.atkPos["atk4"] = (750, 670)
+            self.atkPos["atk5"] = (1210, 385)  # Col 3
+            self.atkPos["atk6"] = (1210, 670)
+            self.atkPos["reroll"] = (1680, 550)  # Reroll
+        self.atkPos["limitbreak"] = (565, 980)  # Limitbreak
+        self.atkPos["endturn"] = (1800, 950)  # End turn
+
     def parseCommande(self, command):
         print("parsing {}".format(command))
         # Commande de la forme:
@@ -87,20 +89,23 @@ class Warrior(Base):
         pyautogui.moveTo(self.dicePos[arg[0]])
         pyautogui.dragTo(self.atkPos[arg[1]], duration=0.2)
 
+
 testRun = Warrior()
 
 # Partie Twitch
 bot = commands.Bot(
-    irc_token = Twitchkeys.IRC_TOKEN,
-    client_id = Twitchkeys.CLIENT_ID,
-    nick = Twitchkeys.NICK,
-    prefix = Twitchkeys.PREFIX,
-    initial_channels = Twitchkeys.INITIAL_CHANNELS
+    irc_token=Twitchkeys.IRC_TOKEN,
+    client_id=Twitchkeys.CLIENT_ID,
+    nick=Twitchkeys.NICK,
+    prefix=Twitchkeys.PREFIX,
+    initial_channels=Twitchkeys.INITIAL_CHANNELS
 )
+
 
 @bot.event
 async def event_ready():
     print("Bot OK")
+
 
 @bot.event
 async def event_message(ctx):
@@ -108,7 +113,7 @@ async def event_message(ctx):
     testRun.parseCommande(ctx.content)
 bot.run()
 
-#while 1:
+# while 1:
 #    cmd = input(">>> ")
 #    try:
 #        testRun.parseCommande(cmd)
